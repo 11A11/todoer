2f091aa3e3d93d53ee12224b40b39734a171424b
Anadi Sharma <anadisharma11041997@gmail.com>
2018-10-30 01:59:23 +0530

    REST api done, mongodb connection success

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var index = require('./routes/index');
+var users = require('./routes/users');
+var api = require('./routes/api.route')
 var app = express();
 
 // view engine setup
@@ -22,7 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/', index);
+app.use('/users', users);
+app.use('/api', api);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
